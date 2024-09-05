37d8a87c73f26d5ad07c556d055cc838f14f8ef3
Florents Tselai <florents.tselai@gmail.com>
2024-09-05 14:31:25 +0300

    Correct syntac

--- sql/jsonb_apply--0.1.0.sql
+++ sql/jsonb_apply--0.1.0.sql
@@ -3,7 +3,7 @@ AS
 'MODULE_PATHNAME'
     LANGUAGE C;
 
-CREATE FUNCTION jsonb_apply_variadic(jsonb, text, variadic params jsonb[]) RETURNS jsonb
+CREATE FUNCTION jsonb_apply_variadic(jsonb, text, variadic "any") RETURNS jsonb
 AS
 'MODULE_PATHNAME'
     LANGUAGE C;
