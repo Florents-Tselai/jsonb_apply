c4f1623b22d897c583b14203c2724df924c39a54
Florents Tselai <florents.tselai@gmail.com>
2024-09-07 15:47:22 +0300

    Null variadic no explicit

--- test/sql/variadic.sql
+++ test/sql/variadic.sql
@@ -1,13 +1,13 @@
 create function lorem() returns text language sql as $$ select 'lorem ipsum'$$;
 
 select jsonb_apply_variadic('"hello"', 'lorem()');
-select jsonb_apply_variadic('"hello"', 'lower(text)', NULL);
-select jsonb_apply_variadic('"hello"', 'upper(text)', NULL);
-select jsonb_apply_variadic('"hello"', 'reverse(text)', NULL);
+select jsonb_apply_variadic('"hello"', 'lower(text)');
+select jsonb_apply_variadic('"hello"', 'upper(text)');
+select jsonb_apply_variadic('"hello"', 'reverse(text)');
 
-select jsonb_apply_variadic('{"f": "John", "l": "Doe", "message": "Who are you?", "arr": ["Hello", {"k": "value"}]}', 'lower(text)', NULL);
-select jsonb_apply_variadic('{"f": "John", "l": "Doe", "message": "Who are you?", "arr": ["Hello", {"k": "value"}]}', 'upper(text)', NULL);
-select jsonb_apply_variadic('{"f": "John", "l": "Doe", "message": "Who are you?", "arr": ["Hello", {"k": "value"}]}', 'reverse(text)', NULL);
+select jsonb_apply_variadic('{"f": "John", "l": "Doe", "message": "Who are you?", "arr": ["Hello", {"k": "value"}]}', 'lower(text)');
+select jsonb_apply_variadic('{"f": "John", "l": "Doe", "message": "Who are you?", "arr": ["Hello", {"k": "value"}]}', 'upper(text)');
+select jsonb_apply_variadic('{"f": "John", "l": "Doe", "message": "Who are you?", "arr": ["Hello", {"k": "value"}]}', 'reverse(text)');
 
 
 select jsonb_apply_variadic('"hello world"', 'replace(text, text, text)', 'hello', 'bye');
